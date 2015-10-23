4891d33c0e265d7fe04ec7eaebbc1e5af5e9f2aa
Amanda <a.vacharat@gmail.com>
2015-10-22 23:04:25 -0400

    makes array of text nodes but changes val instead of html

--- meowify.js
+++ meowify.js
@@ -28,20 +28,65 @@ $.fn.meowify = function () {
 	});
 	$('#meow').attr('id', 'meow0');
 
-	var textNodes = $("body").contents().filter(function(){
-			return (this.nodeType === 3);
-	});
+	function makeCatImg(element) {
+		var text_nodes = [];
+		(function recursiveWalk(node){
+			if (node) {
+				node = node.firstChild;
+				while (node != null) {
+					if (node.nodeType ==3) {
+						text_nodes.push(node);
+					} else if (node.nodeType ==1) {
+						recursiveWalk(node);
+					}
+					node = node.nextSibling;
+				}
+			}
+		})(element);
+console.log(text_nodes)
+
+
+		$.each(text_nodes, function(index, val){
+			var img = '<img style="height:1em;margin:0 1px; margin-top:-3px;" src="img/catface.png" />'
+			if (val.textContent) {
+				var text = val.textContent;
+				var html= text.replace(/\o/g, img);
+				// console.log(val.parentElement.innerHTML)
+				// val.parentElement.innerHTML= html;
+				val.textContent= html;
+				
+				
+				// val.parentElement.replaceWith(html);
+				// val.parentElement.innerHTML = val.textContent.replace(/\o/g, img);
+				// val.textContent= val.textContent.replace(/\o/g, img)
+				// text.html(html);
+				
+				
+			}
+
+			
+		})
+			console.log(text_nodes)
+			
+	}
+
+	makeCatImg(document.body);
+
+
+	// var textNodes = $("body").contents().filter(function(){
+	// 		return (this.nodeType === 3);
+	// });
 
 	//console.log(typeof textNodes);
 	//console.log(textNodes);
 
 	var img = '<img style="height:1em;margin:0 1px; margin-top:-3px;" src="img/catface.png" />';
 
-	Object.keys(textNodes).forEach(function(el){
-		console.log(textNodes[el]);
-		var html = el.textContent.replace(/\o/g, img);
-		$(this).html(html);
-	});
+	// Object.keys(textNodes).forEach(function(el){
+	// 	console.log(textNodes[el]);
+	// 	var html = el.textContent.replace(/\o/g, img);
+	// 	$(this).html(html);
+	// });
 
 	// $('h1, a').each(function() {
 	//     var txt = $(this).text();
