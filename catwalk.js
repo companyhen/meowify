20ab08b665fd55099194abf7cdae1b1024241258
Amanda <a.vacharat@gmail.com>
2015-10-28 14:53:25 -0400

    added fat cat img

--- catwalk.js
+++ catwalk.js
@@ -1,41 +1,35 @@
-// when the page loads, startPaws
-$(document).ready(startPaws());
+// when the page loads, animateCat
+$(document).ready(animateCat());
 
 
-function startPaws() {
-    // append 2 hidden DIVs
-    var $newdiv1 = $('<div id="pawprint1" style="height: 25px; display: none;"><img src="img/balloon.png" /></div>');
-    // var $newdiv2 = $('<div id="pawprint2" style="height: 85px; display: none;"><img src="https://cdn3.iconfinder.com/data/icons/pets-and-animals-1/60/Pawprint-01-128.png" /></div>');
+function animateCat() {
+    var $newdiv1 = $('<div id="catBalloon" style="height: 25px; display: none;"><img src="img/balloon.png" /></div>');
     $('body').append($newdiv1);
-    // move the DIVs
-    movePaws(500);
+    moveCat(500);
 }
 
-function movePaws(count) {
+function moveCat(count) {
     // start positions
     leftpos = -100;
     toppos = 0;
     n = 0;
-    var pawprint1 = document.getElementById("pawprint1");
-    // var pawprint2 = document.getElementById("pawprint2");
-    pawprint1.style.position = "absolute";
-    pawprint1.style.left = -250 + "px";
-    pawprint1.style.top = 0 + "px";
-    // pawprint2.style.position = "absolute";
+    var catBalloon = document.getElementById("catBalloon");
+    catBalloon.style.position = "absolute";
+    catBalloon.style.left = -250 + "px";
+    catBalloon.style.top = 0 + "px";
     moveit();
 
     function moveit() {
         n += 1;
-        pawprint1.style.display = "inherit";
-        pawprint1.style.left = ((parseInt(pawprint1.style.left) || 0) + 5) + "px";
-        pawprint1.style.top = ((parseInt(pawprint1.style.top) || 0) + toppos) + "px";
+        catBalloon.style.display = "inherit";
+        catBalloon.style.left = ((parseInt(catBalloon.style.left) || 0) + 5) + "px";
+        catBalloon.style.top = ((parseInt(catBalloon.style.top) || 0) + toppos) + "px";
 
         if (count-- > 0) {
             setTimeout(moveit, 75);
         }
         else {
-            pawprint1.style.display = "none";
-            // pawprint2.style.display = "none";
+            catBalloon.style.display = "none";            
         }
     }
 }
